80780dd8914b8378f1a0945019c944b638e01a3f
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-01 17:23:48 -0800

    outlined main methods for generate

--- src/main/data/generate.py
+++ src/main/data/generate.py
@@ -1,5 +1,18 @@
-def main():
+dataset_names = ["oracles", "tokens"]
+
+
+def generate(dataset_name: str):
+    """
+    Generates the given dataset and saves the output as a ".pt" file.
+    :param dataset_name: the dataset type
+    """
     pass
+
+
+def main():
+    dataset_name = "oracles"
+    assert dataset_name in dataset_names
+    generate(dataset_name)
 
 
 if __name__ == "__main__":
