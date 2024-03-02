f6b3db7b82d5a7ec75634604b563e90bc0efa8da
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-01 22:10:46 -0800

    added functionality for tokenizing input sequences

---
+++ src/main/data/tokenize.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import torch
+from transformers import CodeLlamaTokenizer
+
+tokenizer = CodeLlamaTokenizer.from_pretrained("codellama/CodeLlama-7b-hf")
+
+
+def tokenize(dataset: List[str]) -> List[torch.Tensor]:
+    """
+    Tokenizes a given set of inputs
+    :param dataset:
+    :return:
+    """
+    dataset_toks = []
+    for query in dataset_toks:
+        dataset_toks.append(tokenizer.tokenize(query))
+    return dataset_toks
