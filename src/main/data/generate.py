cded0c15f1fa3875929097adb3aa0b3ae7a6b98c
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-03 14:20:21 -0800

    uses to_json instead of to_csv

--- src/main/data/generate.py
+++ src/main/data/generate.py
@@ -2,7 +2,7 @@ from os.path import join
 
 from src.main.data.oracles_dataset import get_oracles_dataset
 from src.main.data.tokens_dataset import get_tokens_dataset
-from src.main.util import root_dir, save_dataframe
+from src.main.util import root_dir
 
 # all supported dataset names
 dataset_names = ["oracles", "tokens"]
@@ -21,7 +21,7 @@ def generate_dataset(dataset_name: str):
         raise ValueError(f"Unrecognized dataset name: {dataset_name}")
     print(f"Generated {len(dataset)} datapoints from the {dataset_name} dataset.")
     artifact_name = join(root_dir, "dataset", f"{dataset_name}_dataset.csv")
-    save_dataframe(artifact_name, dataset)
+    dataset.to_json(artifact_name)
 
 
 def main():
