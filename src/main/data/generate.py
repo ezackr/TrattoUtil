f76ceef8931933461a00630d1f32f18b82e9412c
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-03 13:44:49 -0800

    added calls to oracles_dataset and tokens_dataset

--- src/main/data/generate.py
+++ src/main/data/generate.py
@@ -1,18 +1,27 @@
+from src.main.data.oracles_dataset import get_oracles_dataset
+from src.main.data.tokens_dataset import get_tokens_dataset
+
+# all supported dataset names
 dataset_names = ["oracles", "tokens"]
 
 
-def generate(dataset_name: str):
+def generate_dataset(dataset_name: str):
     """
     Generates the given dataset and saves the output as a ".pt" file.
     :param dataset_name: the dataset type
     """
-    pass
+    if dataset_name == "oracles":
+        dataset = get_oracles_dataset()
+    elif dataset_name == "tokens":
+        dataset = get_tokens_dataset()
+    else:
+        raise ValueError(f"Unrecognized dataset name: {dataset_name}")
+    print(len(dataset))
 
 
 def main():
     dataset_name = "oracles"
-    assert dataset_name in dataset_names
-    generate(dataset_name)
+    generate_dataset(dataset_name)
 
 
 if __name__ == "__main__":
