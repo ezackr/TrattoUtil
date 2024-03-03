2aee38ec774ffb891871c32bc9029ca58bd363a7
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-03 14:04:01 -0800

    used safer dataframe saving methods

--- src/main/data/generate.py
+++ src/main/data/generate.py
@@ -2,7 +2,7 @@ from os.path import join
 
 from src.main.data.oracles_dataset import get_oracles_dataset
 from src.main.data.tokens_dataset import get_tokens_dataset
-from src.main.util import root_dir
+from src.main.util import root_dir, save_dataframe
 
 # all supported dataset names
 dataset_names = ["oracles", "tokens"]
@@ -21,7 +21,7 @@ def generate_dataset(dataset_name: str):
         raise ValueError(f"Unrecognized dataset name: {dataset_name}")
     print(f"Generated {len(dataset)} datapoints from the {dataset_name} dataset.")
     artifact_name = join(root_dir, "dataset", f"{dataset_name}_dataset.csv")
-    dataset.to_csv(artifact_name)
+    save_dataframe(artifact_name, dataset)
 
 
 def main():
