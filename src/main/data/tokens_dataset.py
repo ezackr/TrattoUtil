f3d115daf386254977ff6c8488d0a15a5d315dcd
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-19 14:03:05 -0700

    reformatting follows new tokens_dataset structure

--- src/main/data/tokens_dataset.py
+++ src/main/data/tokens_dataset.py
@@ -60,69 +60,39 @@ def _get_next_token(next_possible_tokens: Tuple[str, str, str, str]) -> str:
     raise ValueError("Unable to identify next possible token from list:", next_possible_tokens)
 
 
-def _reformat_token_dp(grouped_token_dp: pd.DataFrame, use_retrieval: bool) -> pd.DataFrame:
+def _reformat_token_dp(raw_token_dp: pd.DataFrame, use_retrieval: bool) -> pd.DataFrame:
     """
     Re-formats a token datapoint into the new format, as described in the
     top-level README.
-    :param grouped_token_dp: token datapoints grouped by "oracleId" and
-    "oracleSoFar"
+    :param raw_token_dp: an original token datapoint
     :return: the re-formatted token datapoint
     """
     # get retrieval information
     if use_retrieval:
-        retrieval_info = _get_retrieval_information(grouped_token_dp["nextPossibleTokens"])
+        retrieval_info = _get_retrieval_information(raw_token_dp["eligibleTokens"])
     else:
         retrieval_info = ""
     # get prompt and label
-    method_javadoc = grouped_token_dp["methodJavadoc"].replace("    /**", "/**")
+    method_javadoc = raw_token_dp["methodJavadoc"].replace("    /**", "/**")
     method_javadoc = re.sub(r"\n[ \t]*\*", "\n *", method_javadoc, flags=re.MULTILINE)
-    method_signature = grouped_token_dp["methodSourceCode"].split("{")[0]
-    assertion_comment = f'// \"{grouped_token_dp["javadocTag"]}\" assertion'
+    method_signature = raw_token_dp["methodSourceCode"].split("{")[0]
+    assertion_comment = f'// \"{raw_token_dp["javadocTag"]}\" assertion'
     assertion_comment = re.sub(r"\n\s*", " ", assertion_comment)
-    next_token = _get_next_token(grouped_token_dp["nextPossibleTokens"])
-    token_values = list({");" if t_info[0] == ";" else t_info[0] for t_info in grouped_token_dp["nextPossibleTokens"]})
-    if next_token == ";" and grouped_token_dp["oracleSoFar"] == "":
+    next_token = raw_token_dp["token"]
+    token_values = list({");" if t_info[0] == ";" else t_info[0] for t_info in raw_token_dp["eligibleTokens"]})
+    if next_token == ";" and raw_token_dp["oracleSoFar"] == "":
         assertion_so_far = ""
         token_values = ["assertTrue(", "// No assertion"]
         label = "// No assertion"
     else:
-        assertion_so_far = f'assertTrue({grouped_token_dp["oracleSoFar"]}'
+        assertion_so_far = f'assertTrue({raw_token_dp["oracleSoFar"]}'
         label = next_token if next_token != ";" else ");"
     random.shuffle(token_values)
     next_possible_tokens_comment = f"// Next possible tokens: {token_values}"
-    grouped_token_dp["prompt"] = retrieval_info + method_javadoc + "\n" + method_signature + " {\n}\n\n" + \
+    raw_token_dp["prompt"] = retrieval_info + method_javadoc + "\n" + method_signature + " {\n}\n\n" + \
         assertion_comment + "\n" + next_possible_tokens_comment + "\n" + assertion_so_far
-    grouped_token_dp["label"] = label
-    return grouped_token_dp[["prompt", "label"]]
-
-
-def _aggregate_grouped_token_dps(grouped_token_dps: pd.DataFrame, oracle_so_far: str) -> pd.DataFrame:
-    """
-    Aggregates a group of original token datapoints into a single datapoint.
-    :param grouped_token_dps: the grouped raw token datapoints, each from the
-    same original oracleId and oracleSoFar features
-    :param oracle_so_far: the shared oracleSoFar feature
-    :return: an aggregated token datapoint
-    """
-    grouped_token_dps = grouped_token_dps.reset_index()
-    # collect all token information
-    token_tuples = []
-    for idx in grouped_token_dps.index:
-        token_tuples.append((
-            grouped_token_dps["token"][idx],
-            grouped_token_dps["tokenClass"][idx],
-            grouped_token_dps["tokenInfo"][idx],
-            grouped_token_dps["label"][idx]
-        ))
-    # aggregate datapoints
-    agg_data = {
-        "javadocTag": grouped_token_dps["javadocTag"][0],
-        "methodJavadoc": grouped_token_dps["methodJavadoc"][0],
-        "methodSourceCode": grouped_token_dps["methodSourceCode"][0],
-        "oracleSoFar": oracle_so_far,
-        "nextPossibleTokens": [token_tuples]
-    }
-    return pd.DataFrame(agg_data)
+    raw_token_dp["label"] = label
+    return raw_token_dp[["prompt", "label"]]
 
 
 def _create_starting_token_dp(token_dp: pd.Series) -> pd.Series:
@@ -189,6 +159,7 @@ def get_tokens_dataset(dataset_dir: str = None, split: str = "train", use_retrie
         for file in tqdm(files):
             abs_path = join(root, file)
             raw_token_dps = _read_raw_token_dps(abs_path)
+            print(raw_token_dps)
             if len(raw_token_dps) > 0:
                 token_dps = raw_token_dps.apply(lambda x: _reformat_token_dp(x, use_retrieval), axis=1)
                 token_dps_list.append(token_dps)
@@ -196,3 +167,7 @@ def get_tokens_dataset(dataset_dir: str = None, split: str = "train", use_retrie
     all_token_dps = _add_starting_token_dps(all_token_dps)
     all_token_dps.drop(columns=["index"], inplace=True)
     return all_token_dps
+
+
+if __name__ == "__main__":
+    get_tokens_dataset(split="validation", use_retrieval=False)
